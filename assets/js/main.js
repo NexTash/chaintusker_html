50e72aedaea44c47d56769ff018935ac8babce31
abdulogs <abdulogs88@gmail.com>
2024-03-12 11:26:54 +0500

    Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,94 +1,3 @@
-// // // Get the custom cursorpointer elements
-// // const cursorpointer = document.querySelector(".cursor");
-// // // Track mouse movement
-// // document.addEventListener("mousemove", (e) => {
-// //     // Update the position of the exact cursorpointer
-// //     if (e.target.matches(".btn-outline") | e.target.matches(".btn")) {
-// //         cursorpointer.classList.add("cursor-text");
-// //         cursorpointer.innerText = "Click";
-// //     } else if (e.target.matches(".image-link")) {
-// //         cursorpointer.classList.add("cursor-text");
-// //         cursorpointer.innerText = "Visit";
-// //     } else if (e.target.matches(".cursor-hover")) {
-// //         const text = e.target.getAttribute("data-text");
-// //         cursorpointer.classList.add("cursor-text");
-// //         cursorpointer.innerText = text;
-// //     } else {
-// //         cursorpointer.classList.remove("cursor-text");
-// //         cursorpointer.innerText = "";
-// //     }
-
-// //     cursorpointer.style.left = e.pageX + "px";
-// //     cursorpointer.style.top = e.pageY + "px";
-// // });
-
-// // Lazy loader
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var lazyImages = document.querySelectorAll(".lazy-load");
-
-//   var options = {
-//     threshold: 0.5, // Adjust threshold as needed
-//   };
-
-//   var lazyLoad = function (entries, observer) {
-//     entries.forEach(function (entry) {
-//       if (entry.isIntersecting) {
-//         var img = entry.target;
-//         img.src = img.dataset.src;
-//         img.classList.add("fade-in");
-//         img.classList.remove("lazy-load");
-//         observer.unobserve(img);
-//       }
-//     });
-//   };
-
-//   var observer = new IntersectionObserver(lazyLoad, options);
-
-//   lazyImages.forEach(function (img) {
-//     observer.observe(img);
-//   });
-// });
-
-// (function () {
-//   var counter = 0;
-//   var c = 0;
-
-//   var i = setInterval(function () {
-//     var counterElement = document.querySelector(
-//       ".loading-page .counter .count"
-//     );
-//     var progressBar = document.querySelector(
-//       ".loading-page .counter .load-progress"
-//     );
-
-//     counterElement.innerHTML = c + "%";
-//     progressBar.style.width = c + "%";
-
-//     counter++;
-//     c++;
-
-//     if (counter == 101) {
-//       clearInterval(i);
-//       hideLoader();
-//     }
-//   }, 10);
-
-//   function hideLoader() {
-//     // Hide the loader smoothly
-//     var loader = document.querySelector(".loading-page");
-//     loader.style.opacity = 0; // Set opacity to 0
-//     setTimeout(function () {
-//       loader.style.display = "none"; // After the transition, hide the loader
-//     }, 500); // Adjust the timeout to match the transition duration (0.5s)
-//   }
-
-//   // Add an event listener for when the page finishes loading
-//   window.onload = function () {
-//     hideLoader();
-//   };
-// })();
-
 document.querySelectorAll(".menu-btn").forEach(function (item) {
   item.addEventListener("click", function () {
     const sidebar = document.getElementById("sidebar");
@@ -108,23 +17,6 @@ document.querySelectorAll(".menu-btn").forEach(function (item) {
   });
 });
 
-// NAVBAR
-window.onscroll = function () {
-  navbar();
-};
-
-function navbar() {
-  let position = 20;
-  if (
-    document.body.scrollTop > position ||
-    document.documentElement.scrollTop > position
-  ) {
-    document.getElementById("navbar").classList.add("navbar-scroll");
-  } else {
-    document.getElementById("navbar").classList.remove("navbar-scroll");
-  }
-}
-
 // Dropdown
 (function () {
   document.addEventListener("click", function (e) {
@@ -140,10 +32,10 @@ function navbar() {
       target.classList.add("active");
       panes.forEach((i) => i.classList.remove("active"));
       document.querySelector(active).classList.add("active");
-    } else if (target.matches('[data-toggle="collapse"]')) {
-      const collapse = target?.closest(".collapse");
-      const items = collapse?.querySelectorAll(".c-item");
-      const active = target?.closest(".c-item");
+    } else if (target.matches('[data-nxt-toggle="collapse"]')) {
+      const collapse = target?.closest(".collapsible");
+      const items = collapse?.querySelectorAll(".collapse-item");
+      const active = target?.closest(".collapse-item");
       if (active) {
         items.forEach((i) => i.classList.remove("active"));
         if (active.classList.contains(".active")) {
